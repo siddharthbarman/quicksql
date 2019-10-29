9eb166d2822ed5bb25a1cd7f2f47be4cf9a40256
siddharthbarman <sbytestream@outlook.com>
2019-10-29 15:54:33 +0530

    Added example for renaming column in WITH and reading node text

--- XML-with-Namespaces.sql
+++ XML-with-Namespaces.sql
@@ -14,6 +14,7 @@ declare @xmlWithoutNs nvarchar(max) = N'
 	</entries>
 </addressBook>';
 
+-- Parsing XML without namespaces
 declare @xml nvarchar(max) = @xmlWithoutNs;
 declare @idoc int
 
@@ -26,6 +27,7 @@ with
 );
 exec sp_xml_removedocument @idoc;
 
+-- Reading XML with namespaces incorrectly
 set @xml = @xmlWithNs;
 exec sp_xml_preparedocument @idoc output, @xml 
 select * from OPENXML(@idoc, N'/addressBook/entries/entry') 
@@ -36,6 +38,7 @@ with
 )
 exec sp_xml_removedocument @idoc;
 
+-- Reading XML with namespaces correctly
 exec sp_xml_preparedocument @idoc output, @xml, N'<root xmlns:d="http://www.tempuri.org/addressBook.xsd" xmlns:e="http://www.tempuri.org/addressBookProperties.xsd"/>' ;
 select * from OPENXML(@idoc, N'/d:addressBook/d:entries/d:entry') 
 with
@@ -44,3 +47,24 @@ with
 	[e:email] varchar(50)
 );
 exec sp_xml_removedocument @idoc;
+
+-- Reading XML with namespaces correctly but renaming the columns and reading the node text
+declare @xmlWithNs nvarchar(max) = N'
+<addressBook xmlns="http://www.tempuri.org/addressBook.xsd">
+    <entries xmlns:e="http://www.tempuri.org/addressBookProperties.xsd">
+        <entry e:id="1" e:email="siddharthbarman@email.com">Siddharth Barman</entry>
+        <entry e:id="2" e:email="sajankumar@email.com">Sajan Kumar</entry>
+    </entries>
+</addressBook>';
+
+declare @idoc int;
+exec sp_xml_preparedocument @idoc output, @xmlWithNs, N'<root xmlns:d="http://www.tempuri.org/addressBook.xsd" xmlns:e="http://www.tempuri.org/addressBookProperties.xsd"/>' ;
+
+select * from OPENXML(@idoc, N'/d:addressBook/d:entries/d:entry') 
+with
+(
+    ID int '@e:id',
+    FullName varchar(50) 'text()',
+	EmailID varchar(50) '@e:email'
+);
+exec sp_xml_removedocument @idoc;    
